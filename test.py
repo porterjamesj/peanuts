43e5bbd108e379b4fe62dd5b90e3e71e03cf55a5
James Porter <james@jamesporter.me>
2017-05-12 11:44:18 -0500

    playing wav files working
    turns out the trick was using a larger blocksize

--- test.py
+++ test.py
@@ -1,26 +1,53 @@
 #! /usr/bin/env python
 
 from pysoundcard import Stream
+from soundfile import blocks
 from webrtcvad import Vad
+
+from itertools import cycle
+
 
 SAMPLE_RATE = 32000
 HOP_S = 320  # this is the number of frames in 10 ms
 
-s = Stream(
-    blocksize=HOP_S,  # TODO not sure what this does
-    channels=1,
-    dtype='int16',
-    samplerate=SAMPLE_RATE
-)
 
-v = Vad(2)
-
-s.start()
+def open_mic_stream():
+    return Stream(
+        blocksize=HOP_S,  # TODO not sure what this does
+        channels=1,
+        dtype='int16',
+        samplerate=SAMPLE_RATE
+    )
 
 
-while True:
-    vec = s.read(HOP_S)
-    print(v.is_speech(vec.tostring(), SAMPLE_RATE))
+def open_speaker_stream():
+    return Stream(44100)
 
-    if len(vec) < HOP_S:
-        break
+
+def infinite_teacher_noise():
+    return cycle(blocks(
+        "teacher.wav",
+        1000000
+    ))
+
+
+def main():
+    mic = open_mic_stream()
+    speak = open_speaker_stream()
+    vad = Vad(3)  # TODO maybe 3?
+
+    speak.start()
+    # soundcard.start()
+
+    for noise in infinite_teacher_noise():
+        # sample = soundcard.read(HOP_S).tostring()
+        speak.write(noise)
+        # print(vad.is_speech(sample, SAMPLE_RATE))
+
+        # # TODO hmmmm, probably remove
+        # if len(sample) < HOP_S:
+        #     break
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_output.py
@@ -0,0 +1,16 @@
+import sys
+import numpy as np
+from scipy.io.wavfile import read as wavread
+from pysoundcard import Stream
+
+"""Play an audio file."""
+
+fs, wave = wavread(sys.argv[1])
+wave = np.array(wave, dtype=np.float64)
+wave /= 2**15 # normalize -max_int16..max_int16 to -1..1
+
+blocksize = 1024
+s = Stream(samplerate=fs, blocksize=blocksize)
+s.start()
+s.write(wave)
+s.stop()
