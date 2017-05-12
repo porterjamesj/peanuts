aa0ad4aa4df53009697908685bb8930875d5140c
James Porter <james@jamesporter.me>
2017-05-12 12:26:09 -0500

    sort of doing both at the same time
    I think this will work better if I convert it to use the callback API

--- test.py
+++ test.py
@@ -2,6 +2,8 @@
 
 from pysoundcard import Stream
 from webrtcvad import Vad
+from scipy.io.wavfile import read as wavread
+import numpy as np
 
 from itertools import cycle
 
@@ -9,43 +11,35 @@ from itertools import cycle
 SAMPLE_RATE = 32000
 HOP_S = 320  # this is the number of frames in 10 ms
 
+_sr, wave = wavread("teacher.wav")
+wave = np.array(wave, dtype=np.int16)
+
 
 def open_mic_stream():
     return Stream(
-        blocksize=HOP_S,  # TODO not sure what this does
+        blocksize=256,  # TODO not sure what this does
         channels=1,
         dtype='int16',
-        samplerate=SAMPLE_RATE
+        samplerate=SAMPLE_RATE,
     )
-
-
-def open_speaker_stream():
-    return Stream(44100)
-
-
-def infinite_teacher_noise():
-    return cycle(blocks(
-        "teacher.wav",
-        1000000
-    ))
 
 
 def main():
     mic = open_mic_stream()
-    speak = open_speaker_stream()
-    vad = Vad(3)  # TODO maybe 3?
+    vad = Vad(3)
 
-    speak.start()
-    # soundcard.start()
+    mic.start()
 
-    for noise in infinite_teacher_noise():
-        # sample = soundcard.read(HOP_S).tostring()
-        speak.write(noise)
-        # print(vad.is_speech(sample, SAMPLE_RATE))
+    i = 0
+    while True:
+        mic.write(wave[i:i+1024])
+        i += 1024
+        sample = mic.read(HOP_S).tostring()
+        print(vad.is_speech(sample, SAMPLE_RATE))
 
-        # # TODO hmmmm, probably remove
-        # if len(sample) < HOP_S:
-        #     break
+        # TODO hmmmm, probably remove
+        if len(sample) < HOP_S:
+            break
 
 
 if __name__ == "__main__":
