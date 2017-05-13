035f425a9663ba708550245f64ca0ba5a609e844
James Porter <james@jamesporter.me>
2017-05-13 02:30:53 -0500

    clean up a bit

--- test.py
+++ test.py
@@ -13,11 +13,44 @@ from itertools import cycle, islice
 SAMPLE_RATE = 32000
 HOP_S = 320  # this is the number of frames in 10 ms
 
-_sr, wave = wavread("teacher.wav")
-wave = np.array(wave, dtype=np.int16)
-wave = wave[:, 0:1]  # turn to mono by just picking one channel lol
 
-forever_wave = cycle(wave)
+# onset params
+LAG_TIME = 5  # in ms
+NECESSARY_FRACTION = 0.3
+
+
+def open_wav(name="teacher.wav"):
+    sr, wave = wavread(name)
+    assert sr == SAMPLE_RATE
+    wav = np.array(wave, dtype=np.int16)
+    wav = (wav[:, 0:1] + wav[:, 1:2]) / 2
+    return wav
+
+
+class VoiceOver(object):
+    """
+    Bag of state for tracking stuff in the stream callback.
+    """
+
+    def __init__(self):
+        self.input_buf = np.empty((256, 1), dtype=np.int16)
+        self.vad = Vad(3)
+        self.vad_q = deque([False], LAG_TIME)
+        self.output = cycle(open_wav())
+
+    def callback(self, in_data, out_data, time_info, status):
+        self.input_buf = np.concatenate((self.input_buf, in_data))
+        if len(self.input_buf) > HOP_S:  # we can pass data to vad
+            ten_ms, rest = self.input_buf[0:HOP_S], self.input_buf[HOP_S:]
+            self.vad_q.append(
+                self.vad.is_speech(ten_ms.tostring(), SAMPLE_RATE)
+            )
+            self.input_buf = rest
+        if sum(self.vad_q) > len(self.vad_q)*NECESSARY_FRACTION:
+            out_data[:] = list(islice(self.output, len(out_data)))
+        else:
+            out_data[:] = np.zeros(out_data.shape)
+        return continue_flag
 
 
 def open_mic_stream():
@@ -29,35 +62,18 @@ def open_mic_stream():
     )
 
 
-buf = np.empty((256, 1), dtype=np.int16)
-vad = Vad(3)
-vad_q = deque([False], 5)
-
-
-def callback(in_data, out_data, time_info, status):
-    global buf  # TODO yuk
-    buf = np.concatenate((buf, in_data))
-    if len(buf) > HOP_S:  # we can pass data to vad
-        ten_ms, rest = buf[0:HOP_S], buf[HOP_S:]
-        vad_q.append(vad.is_speech(ten_ms.tostring(), SAMPLE_RATE))
-        buf = rest
-    if sum(vad_q) > len(vad_q)*0.3:
-        out_data[:] = list(islice(forever_wave, len(out_data)))
-    else:
-        out_data[:] = np.zeros(out_data.shape)
-    return continue_flag
-
-
 def main():
+    vo = VoiceOver()
     s = Stream(
         blocksize=256,  # TODO not sure what this does
         channels=1,
         dtype='int16',
         samplerate=SAMPLE_RATE,
-        callback=callback
+        callback=vo.callback,
     )
     s.start()
-    time.sleep(10000)
+    while True:
+        time.sleep(5)
     s.stop()
 
 
